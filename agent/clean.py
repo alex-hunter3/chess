fa2dafb3c5fbb528eaf4308c3fc89bdd403d3f1e
Alexander Hunter <alexrhunter@btinternet.com>
2022-03-29 09:52:57 +0100

    Fix grouping in clean.py

--- agent/clean.py
+++ agent/clean.py
@@ -1,8 +1,40 @@
 import json
 import os
+import re
 
-PATH = os.path.join(os.getcwd(), "agent", "openings.json")
 
-with open(PATH, "r") as f:
-    openings = json.load(f)
+def open_file():
+    PATH = os.path.join(os.getcwd(), "agent", "openings.json")
 
+    with open(PATH, "r") as f:
+        openings = json.load(f)
+
+    return openings
+
+
+def group(openings):
+    grouped = {}
+    current_opening = ""
+
+    for opening in openings:
+        if opening["name"] != current_opening:
+            current_opening = opening["name"]
+            # grouped[current_opening] = []
+
+        moves = []
+
+        for move in opening["moves"].split(" "):
+            if not move[0].isnumeric():
+                moves.append(move)
+
+        grouped[current_opening] = moves
+
+    return grouped
+
+
+if __name__ == "__main__":
+    openings = open_file()
+    openings = group(openings)
+
+    for opening in openings:
+        print(openings[opening])
